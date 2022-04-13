1b4095b88ed1cb8ac54cf9f73c058147cda10d0c
Martyna Król <martyna.maria.krol@gmail.com>
2022-04-13 19:42:16 +0200

    initAccordion almost finished

--- src/js/script.js
+++ src/js/script.js
@@ -55,27 +55,20 @@
   const app = {
     initMenu: function(){
       const thisApp = this;
-      
-      console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);  // twoarzy nowy obiekt na bazie klasy?
+        new Product(productData, thisApp.data.products[productData]);
       }
     },
     
     initData: function(){
       const thisApp = this;
 
-      thisApp.data = dataSource; // skąd on wie tutaj, że ma pobierać dane z innego pliku?
+      thisApp.data = dataSource;
     },
 
     init: function(){
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
@@ -88,13 +81,12 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
-
-      console.log('new Product:', thisProduct);
+      thisProduct.initAccordion();
     }
 
     renderInMenu(){
       const thisProduct = this;
-
+      
       /* generate HTML based on template */
       const generatedHTML = templates.menuProduct(thisProduct.data);
 
@@ -107,6 +99,33 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
 
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+  
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+        const activeProducts = thisProduct.element.querySelectorAll(select.all.menuProductsActive);
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        for(let activeProduct of activeProducts){
+          if(activeProduct !== thisProduct.element){
+            thisProduct.element.classList.remove(classNames.menuProduct.wrapperActive);
+          }
+        }
+
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
 
