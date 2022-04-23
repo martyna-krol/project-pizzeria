7ec2e42e49b4f3fd71353a4fa429256e57e33efe
Martyna Król <martyna.maria.krol@gmail.com>
2022-04-23 16:46:01 +0200

    initAccordion finished

--- src/js/script.js
+++ src/js/script.js
@@ -331,7 +331,7 @@
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         for(let activeProduct of activeProducts){
           if(activeProduct !== thisProduct.element){
-            thisProduct.element.classList.remove(classNames.menuProduct.wrapperActive);
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
           }
         }
 
