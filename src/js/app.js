8bb5defc8aa09ff46a25090e4574865440fd617c
Martyna Król <martyna.maria.krol@gmail.com>
2022-05-27 19:40:10 +0200

    Booked tables visible in website

--- src/js/app.js
+++ src/js/app.js
@@ -77,8 +77,6 @@ const app = {
         return rawResponse.json();
       })
       .then(function(parsedResponse){
-        console.log('parsedResponse', parsedResponse);
-
         /* save parsedResponse as thisApp.data.products */
         thisApp.data.products = parsedResponse;
         /* execute initMenu method */
